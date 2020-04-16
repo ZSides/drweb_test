9965b86a46da0249c4550cc0d9e622c4b19abdae
Ivan Lebedev <Ivan.Lebedev@rosbank.ru>
2020-04-16 10:43:31 +0300

    task 3: rewrited

--- main3.cpp
+++ main3.cpp
@@ -1,5 +1,21 @@
 #include <bits/stdc++.h>
 
+void join(const std::vector<int> &from, std::vector<int> &to) {
+    to.resize(to.size() + from.size());
+    auto f_ptr = from.rbegin();
+    auto t_ptr = to.rbegin() + from.size(), i_ptr = to.rbegin();
+    while (f_ptr != from.rend()) {
+        if (*t_ptr > *f_ptr) {
+            *i_ptr = *t_ptr;
+            t_ptr++;
+        } else {
+            *i_ptr = *f_ptr;
+            f_ptr++;
+        }
+        i_ptr++;
+    }
+}
+
 bool test(const std::function<void(const std::vector<int> &, std::vector<int> &)>& f, const std::vector<int> &a, std::vector<int> &b, const std::vector<int>& expected) {
     f(a, b);
     if (expected != b) {
@@ -18,21 +34,6 @@ bool test(const std::function<void(const std::vector<int> &, std::vector<int> &)
     return expected == b;
 }
 
-void join(const std::vector<int> &from, std::vector<int> &to) {
-    size_t t_ptr = to.size() - 1, f_ptr = from.size() - 1, i_ptr = from.size() + to.size() - 1;
-    to.resize(to.size() + from.size());
-    while (f_ptr != size_t (-1)) {
-        if (to[t_ptr] > from[f_ptr]) {
-            to[i_ptr] = to[t_ptr];
-            t_ptr--;
-        } else {
-            to[i_ptr] = from[f_ptr];
-            f_ptr--;
-        }
-        i_ptr--;
-    }
-}
-
 int main() {
     std::vector<int> f = {1, 4, 6, 8};
     std::vector<int> t = {2, 3, 6, 7, 10};
@@ -40,6 +41,15 @@ int main() {
     f = {3, 5, 7};
     t = {4, 6};
     test(join, f, t, {3, 4, 5, 6, 7});
+    f = {};
+    t = {-6, -4};
+    test(join, f, t, {-6, -4});
+    f = {3, 5, 7};
+    t = {};
+    test(join, f, t, {3, 5, 7});
+    f = {};
+    t = {};
+    test(join, f, t, {});
     return 0;
 }
 
